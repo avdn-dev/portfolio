399f5bf3676972212378b7d8fef9637f189d4aba
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-09 18:17:48 +1100

    Add Hero component

--- src/NavigationHeader.jsx
+++ src/NavigationHeader.jsx
@@ -2,6 +2,7 @@
 // - Update logo image
 // - Update navigation links
 // - Update styling
+// - Make mobile menu disappear?
 
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
@@ -13,37 +14,44 @@ const navigation = [
   { name: 'Contact', href: '#' },
 ]
 
-export default function NavigationHeader() {
+export default function NavigationHeader () {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <header>
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 sm:px-8" aria-label="Global">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 sm:px-8"
+        aria-label="Global">
         <a href="#" className="-m-1.5 p-1.5">
           <span className="sr-only">Anh Nguyen</span>
-          <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          <img className="h-8 w-auto"
+               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+               alt=""/>
         </a>
         <div className="flex sm:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
           </button>
         </div>
         <div className="hidden sm:flex sm:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+            <a key={item.name} href={item.href}
+               className="text-sm font-semibold leading-6">
               {item.name}
             </a>
           ))}
         </div>
       </nav>
-      <Dialog as="div" className="small:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+      <Dialog as="div" className="small:hidden" open={mobileMenuOpen}
+              onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10"/>
+        <Dialog.Panel
+          className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-800 bg-opacity-70 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Anh Nguyen</span>
@@ -55,11 +63,11 @@ export default function NavigationHeader() {
             </a>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
             </button>
           </div>
           <div className="mt-6 flow-root">
@@ -69,7 +77,7 @@ export default function NavigationHeader() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7"
                   >
                     {item.name}
                   </a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NavigationHeader from './NavigationHeader.jsx'
+import Hero from './Hero.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavigationHeader />
+    <NavigationHeader/>
+    <Hero/>
   </React.StrictMode>,
 )
