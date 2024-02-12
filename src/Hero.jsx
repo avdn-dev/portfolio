1edbd945d01d989c9a7829f9641c3a2f8c534661
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-12 22:28:39 +1100

    Add Logo and Footer components

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,23 +1,21 @@
-export default function Example () {
+export default function Hero () {
   return (
-    <div>
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-center">
-            <h1
-              className="text-4xl font-bold tracking-tight sm:text-6xl">
-              Hi! I'm Anh, a totally rad chad
-            </h1>
-            <p className="mt-6 text-lg leading-8">
-              I've got experience in a bunch of stuff, and I'm really good at
-              it. I'm also a really cool person, so you should hire me.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#"
-                 className="text-sm font-semibold leading-6">
-                See more <span aria-hidden="true">→</span>
-              </a>
-            </div>
+    <div className="flex-grow relative isolate px-6 pt-14 lg:px-8">
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="text-center">
+          <h1
+            className="text-4xl font-bold tracking-tight sm:text-6xl">
+            Hi! I'm Anh, a totally rad chad
+          </h1>
+          <p className="mt-6 text-lg leading-8">
+            I've got experience in a bunch of stuff, and I'm really good at
+            it. I'm also a really cool person, so you should hire me.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a href="#"
+               className="text-sm font-semibold leading-6">
+              See more <span aria-hidden="true">→</span>
+            </a>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import NavigationHeader from './NavigationHeader.jsx'
 import Hero from './Hero.jsx'
 import './index.css'
+import Footer from './Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavigationHeader/>
     <Hero/>
+    <Footer/>
   </React.StrictMode>,
 )
