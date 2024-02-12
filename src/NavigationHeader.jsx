293ce61a8878a58ff5a86fbb5a0adc58cd8ff006
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-12 23:04:46 +1100

    Move contact links from Footer to NavigationHeader

--- src/NavigationHeader.jsx
+++ src/NavigationHeader.jsx
@@ -9,25 +9,42 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Logo from './Logo.jsx'
 
-const navigation = [
+const navigationLinks = [
   { name: 'Projects', href: '#' },
   { name: 'About me', href: '#' },
   { name: 'Contact', href: '#' },
 ]
 
-export default function NavigationHeader () {
+const contactLinks = [
+  {
+    name: 'GitHub',
+    href: 'https://github.com/4nhus',
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+]
+
+export default function Example () {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <header>
-      <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 sm:px-8"
-        aria-label="Global">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Anh Viet Duc Nguyen Logo</span>
-          <Logo/>
-        </a>
-        <div className="flex sm:hidden">
+      <nav className="flex items-center justify-between p-6 lg:px-8"
+           aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Anh Viet Duc Nguyen Logo</span>
+            <Logo/>
+          </a>
+        </div>
+        <div className="flex md:hidden">
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
@@ -37,20 +54,30 @@ export default function NavigationHeader () {
             <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
           </button>
         </div>
-        <div className="hidden sm:flex sm:gap-x-12">
-          {navigation.map((item) => (
+        <div className="hidden md:flex md:gap-x-12">
+          {navigationLinks.map((item) => (
             <a key={item.name} href={item.href}
                className="text-sm font-semibold leading-6">
               {item.name}
             </a>
           ))}
         </div>
+        <div className="hidden md:flex md:flex-1 md:justify-end">
+          <div className="flex justify-center space-x-6 md:order-2">
+            {contactLinks.map((item) => (
+              <a key={item.name} href={item.href}>
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true"/>
+              </a>
+            ))}
+          </div>
+        </div>
       </nav>
-      <Dialog as="div" className="small:hidden" open={mobileMenuOpen}
+      <Dialog as="div" className="md:hidden" open={mobileMenuOpen}
               onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10"/>
         <Dialog.Panel
-          className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-800 bg-opacity-70 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Anh Viet Duc Nguyen Logo</span>
@@ -68,15 +95,25 @@ export default function NavigationHeader () {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                {navigation.map((item) => (
+                {navigationLinks.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50"
                   >
                     {item.name}
                   </a>
                 ))}
+              </div>
+              <div className="py-6">
+                <div className="flex justify-center space-x-6 md:order-2">
+                  {contactLinks.map((item) => (
+                    <a key={item.name} href={item.href}>
+                      <span className="sr-only">{item.name}</span>
+                      <item.icon className="h-6 w-6" aria-hidden="true"/>
+                    </a>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
