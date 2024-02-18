4add0ef6ff7160bfed9615bd24a25991e9c2638a
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-18 12:27:15 +1100

    Add technology stack to Footer component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer () {
         className="mx-auto p-6">
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5">
-            &copy; 2024 Anh Nguyen.
+            &copy; 2024 Anh Nguyen. Built with React, p5.js, Tailwind, and Vite.
           </p>
         </div>
       </div>
