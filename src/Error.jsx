a743554acac1259a5f825b09cf7fd7c7940b3537
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-03-15 09:29:16 +1100

    Center hero sections in Hero and Error components, update copy and add animation to Error component

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,8 +1,3 @@
-// TODO:
-// Make error section responsive (make the splash centered)
-// Add animation to the go home button
-// Finalise copy
-
 import Footer from './Footer.jsx'
 import { Link } from 'react-router-dom'
 import NavigationHeader from './NavigationHeader.jsx'
@@ -11,22 +6,21 @@ export default function Error () {
   return (
     <>
       <NavigationHeader/>
-      <main className="flex flex-grow relative isolate min-h-full">
-        <div
-          className="self-center mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-          <p className="text-base font-semibold leading-8 text-white">404</p>
-          <h1
-            className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page
-            not found</h1>
-          <p
-            className="mt-4 text-base text-white/70 sm:mt-6">Uh oh
-            spaghettio</p>
-          <div className="mt-10 flex justify-center">
-            <Link to="/" className="text-sm font-semibold leading-7 text-white">
+      <main
+        className="flex flex-col text-center flex-grow relative px-8 justify-center items-center">
+
+        <p className="text-base font-semibold leading-8">404</p>
+        <h1
+          className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Page
+          not found</h1>
+        <p
+          className="mt-4 text-base text-white/70 sm:mt-6">Nothing to see here
+          ¯\_(ツ)_/¯</p>
+        <div className="mt-10 flex justify-center">
+          <Link to="/" className="text-sm font-semibold leading-7 underline">
               <span
                 aria-hidden="true">&larr;</span> {'There\'s no place like home...'}
-            </Link>
-          </div>
+          </Link>
         </div>
       </main>
       <Footer/>
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,31 +1,27 @@
 // TODO:
-// Make hero section responsive (make the splash centered)
-// Add animation to the see more button
 // Finalise copy
+// Add text censor effect to experiences
 
 import { Link } from 'react-router-dom'
 
 export default function Hero () {
   return (
-    <div className="flex-grow relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="text-center">
-          <h1
-            className="text-4xl font-bold tracking-tight sm:text-6xl">
-            Hi, I'm Anh!
-          </h1>
-          <p className="mt-6 text-lg leading-8">
-            I'm a final year computer science student with experience in
-            backend, web, mobile development and UI/UX design.
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link to="projects"
-                  className="text-sm font-semibold leading-6">
-              See my work <span aria-hidden="true">→</span>
-            </Link>
-          </div>
-        </div>
+    <main
+      className="flex flex-col text-center flex-grow relative px-8 justify-center items-center">
+      <h1
+        className="text-4xl font-bold tracking-tight sm:text-6xl">
+        Hi, I'm Anh!
+      </h1>
+      <p className="mt-6 text-lg leading-8">
+        I'm a final year computer science student with experience in
+        backend, web, mobile development and UI/UX design.
+      </p>
+      <div className="mt-10 flex items-center justify-center">
+        <Link to="projects"
+              className="text-sm font-semibold leading-6 underline">
+          See my work <span aria-hidden="true">→</span>
+        </Link>
       </div>
-    </div>
+    </main>
   )
 }
