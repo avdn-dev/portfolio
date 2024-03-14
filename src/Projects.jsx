e7335179e046750522a8f077698904c4972f19fb
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-03-14 22:18:51 +1100

    Add hover effect to each project card, fix sizing and position of project cards

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,5 @@
 // TODO:
-// - Fix padding around project cards
 // - Change photos displayed
-// - Add hover effect for each project
 // - Add a quick link for each github repo
 // - Add transitions for cards
 
@@ -11,25 +9,21 @@ import projects from './projects.js'
 export default function Projects () {
   return (
     <ul role="list"
-        className="flex-grow grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        className="m-auto flex-grow grid gap-6 px-6 sm:grid-cols-2 sm:gap-8 sm:px-8 lg:grid-cols-3 lg:gap-10 lg:px-10 xl:grid-cols-4 xl:gap-12 xl:px-12">
       {projects.map(project => (
-        <li
-          key={project.name}
-          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg text-center shadow backdrop-blur bg-gray-800/10 ring-1 ring-white/10"
-        >
-          <Link to={project.route}>
-            <div className="flex flex-1 flex-col p-8">
-              <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
-                   src={project.picture} alt=""/>
-              <h3
-                className="mt-6 text-sm font-medium">{project.name}</h3>
-              <dl className="mt-1 flex flex-grow flex-col justify-between">
-                <dt className="sr-only">Title</dt>
-                <dd className="text-sm">{project.description}</dd>
-              </dl>
-            </div>
-          </Link>
-        </li>
+        <Link to={project.route}
+              className="min-w-96 sm:min-w-72  col-span-1 flex overflow-hidden max-w-mbd flex-col divide-y rounded-lg text-center shadow backdrop-lur bg-gray-800/5 ring-1 ring-white/10 transition-all duration-500 ease-in-out transform hover:scale-105 hover:ring-2 hover:ring-white"
+              key={project.name}>
+          <div className="flex flex-1 flex-col">
+            <img className="mx-auto w-full"
+                 src={project.picture}
+                 alt=""/>
+            <h2
+              className="text-2xl mt-4 mb-2 font-medium sm:mt-6 sm:mb-3 lg:mt-8 lg:mb-4">{project.name}</h2>
+            <p
+              className="mb-4 mx-4 text-left sm:text-lg sm:mb-6 sm:mx-6 lg:mb-8 lg:mx-8">{project.description}</p>
+          </div>
+        </Link>
       ))}
     </ul>
   )
